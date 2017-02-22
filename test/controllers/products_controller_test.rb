b0168ce24956228ef8edb33133d0dac9e89d7af2
Rija Aamir <aamir.rija@gmail.com>
2017-02-22 19:45:02 +0000

    Updated Depot

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,3 +1,11 @@
+#---
+# Excerpted from "Agile Web Development with Rails 5",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/rails5 for more book information.
+#---
 require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
@@ -29,8 +37,6 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to product_url(Product.last)
   end
 
-  # ...
-
   test "should show product" do
     get product_url(@product)
     assert_response :success
@@ -46,7 +52,13 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to product_url(@product)
   end
 
-  # ...
+  test "can't delete product in cart" do
+    assert_difference('Product.count', 0) do
+      delete product_url(products(:two))
+    end
+
+    assert_redirected_to products_url
+  end
 
   test "should destroy product" do
     assert_difference('Product.count', -1) do
